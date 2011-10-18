d0991b12b649695b5d0b1633a26502f2cee773ff
jamiehill <jamie@tiltdigital.com>
2011-10-18 17:33:31 +0100

    AIR Digital signing amends

--- SendInputLib/bin/generate_certificate.sh
+++
@@ -1,1 +0,0 @@
-/Applications/Adobe\ Flash\ Builder\ 4.5/sdks/flex_sdk_4.5.1.21328_air3/bin/adt -certificate -cn SelfSign -ou QE -o "Tilt Digital" -c US 2048-RSA sendInput.p12 test
--- SendInputLib/bin/package_osx.sh
+++ SendInputLib/bin/package_osx.sh
@@ -1,4 +1,5 @@
 #
 echo Starting build of SendInputLib.swc - `date`
+
 cp ../../FRESendInput/Debug/FRESendInput.dll .
 /Applications/Adobe\ Flash\ Builder\ 4.5/sdks/flex_sdk_4.5.1.21328_air3/bin/adt -package -storetype pkcs12 -keystore sendInput.p12 -storepass test -target ane SendInputLib.ane extension.xml -swc SendInputLib.swc -platform Windows-x86 library.swf FRESendInput.dll
